4da303706b7adf70c977f47fff440e04c3bdd444
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-09-01 18:37:05 +0100

    ran mvn test

--- contract/src/test/java/com/payment/service/contract/ContractApplicationTests.java
+++ contract/src/test/java/com/payment/service/contract/ContractApplicationTests.java
@@ -2,8 +2,9 @@ package com.payment.service.contract;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(classes = ContractApplication.class)
 class ContractApplicationTests {
 
 	@Test
